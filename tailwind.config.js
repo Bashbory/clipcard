4b018f8c79f670633277875d1fc613f5dd9f1d45
Bashbory <eightmick912@gmail.com>
2023-06-17 13:50:53 +0300

    clip

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+"./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'bai': ['"Bai Jamjuree"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
